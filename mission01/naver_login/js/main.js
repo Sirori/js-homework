61dbb1a2dcdf2e979f1d064f3bd3c23db02251ac
Sirori <tldus7519@gmail.com>
2023-07-16 19:43:47 +0900

    로그인페이지 완성

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -24,13 +24,58 @@ function pwReg(text){
   return re.test(String(text).toLowerCase());
 }
 
+const email = document.querySelector('.user-email-input');
+const pw = document.querySelector('.user-password-input');
+const button = document.querySelector('.btn-login');
+const idError = document.querySelector('#userEmailError')
+const pwError = document.querySelector('#userPasswordError')
 
+let emailValue = email.value;
+let pwValue = pw.value;
+let emailPass = false;
+let pwPass = false;
 
+email.addEventListener('input', function(){
+  emailValue = email.value;
+  console.log(emailValue);
 
+  if(emailReg(emailValue)){
+    //this.classList.add('is-active');
+    idError.classList.remove('is--invalid')
 
+    emailPass = true;
+  }else{
+    //this.classList.remove('is-active');
+    idError.classList.add('is--invalid')
 
+    emailPass = false;
+  }
+  return emailPass;
+})
 
+pw.addEventListener('input', function(){
+  pwValue = pw.value
+  console.log(pwValue);
+  if(pwReg(pwValue)){
+    //this.classList.add('is-active');
+    pwError.classList.remove('is--invalid')
 
+    pwPass = true;
+  }else{
+    //this.classList.remove('is-active');
+    pwError.classList.add('is--invalid')
 
+    pwPass = false
+  }
+  return pwPass;
+})
 
-
+button.addEventListener('click', (e) =>{
+  e.preventDefault();
+  if(emailPass && pwPass){
+    if(user.id === emailValue && user.pw === pwValue){
+      console.log('로그인');
+      window.location.href = 'welcome.html';
+    }
+  }
+})
